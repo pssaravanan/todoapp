3aad75ed872da30afa8c264e0e2771e996d77fdf
pssaravanan <saravananselvamani@gmail.com>
2012-07-03 01:15:21 +0530

    Created todo App & configured south

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'todoapp',                      # Or path to database file if using sqlite3.
+        'USER': 'bluedba',                      # Not used with sqlite3.
+        'PASSWORD': 'bluedba',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'todo',
+    'south'
 )
 
 # A sample logging configuration. The only tangible logging
