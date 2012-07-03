46fc619f5b1cc20d2aacc8f6cfdb64d87d3dbc85
pssaravanan <saravananselvamani@gmail.com>
2012-07-04 01:11:10 +0530

    Validatation test for the model

--- todoapp/todo/tests.py
+++ todoapp/todo/tests.py
@@ -5,12 +5,17 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.utils import unittest
+from todo.models import Event
+from django.core.exceptions import ValidationError
 
+class Todo(unittest.TestCase):
+  def assert_todo_object_should_not_save_with_validation_error(self):
+        self.event = Event(title="some_title", description="some description")
+        self.assertRaises(ValidationError, self.event.clean())
 
-class Todo(TestCase):
   def assert_todo_object_save_with_no_validation_error(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        
+        self.event = Event(title = "Some title", description="some description",
+                           people_wants_to_meet="saravanan",
+                           time_of_event="2012-12-18")
+        self.assertIsNone(self.event.full_clean())
